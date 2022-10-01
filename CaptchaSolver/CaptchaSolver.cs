1206078bd842cd3083f81f9da8b5753e63e86416
vladvasiliev1 <vlad.vasiliev292001@gmail.com>
2022-10-01 17:31:38 +0300

    End write lib

--- CaptchaSolver/CaptchaSolver.cs
+++ CaptchaSolver/CaptchaSolver.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Leaf.xNet;
+using System.Threading;
 
 namespace CaptchaSolver
 {
@@ -16,7 +18,50 @@ namespace CaptchaSolver
             this.apiKey = apiKey;
         }
 
+        public string Solve(string siteKey,string url)
+        {
+            try
+            {
+                HttpRequest httpRequest = new HttpRequest();
 
+                string response = httpRequest.Get($"http://rucaptcha.com/in.php?key={apiKey}&method=userrecaptcha&googlekey={siteKey}&pageurl={url}").ToString();
+
+                int id;
+
+                if (response.ToLower().Contains("ok"))
+                {
+                    id = int.Parse(response.Split('|')[1]);
+
+                    m1:
+                    Thread.Sleep(20000);
+
+                    response = httpRequest.Get($"http://rucaptcha.com/res.php?key={apiKey}&action=get&id={id}").ToString();
+
+                    if (response.ToLower().Contains("ok"))
+                    {
+                        return response.Split('|')[1];
+                    }
+                    else
+                    {
+                        Thread.Sleep(10000);
+
+                        goto m1;
+                    }
+                }
+
+                
+
+                
+            }
+            catch
+            {
+
+            }
+
+            return null;
+        }
+
+        
 
     }
 }
